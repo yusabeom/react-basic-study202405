9010a1d20cebf226f740c1966dc3f091f0617950
sabeom <tkqja717@naver.com>
2024-05-01 16:13:13 +0900

    1. React 초기 설정 완료 및 Component 개념 확인

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './NoName.js';
+import NoName from './NoName.js';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+  const $h2 = <h2>반갑습니다~</h2>
+
+  return ( //하나의 태그만 리턴할 수 있음 하나의 부모태그로 감싸야 함! (빈태그 가능)
+   <> 
+      <NoName />
+     <div className='App'> {/* classNmae으로 클래스 부여 */}
+        <h1>메롱메롱</h1>
+        {$h2}
+     </div>
+     <div className='noname'>
+      <input type='text' />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          오늘은 5월 1일 수요일 입니다. <br/> {/* 닫는 태그가 필수 */}
+          오후 3시 30분 입니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     </div>
+   </>
   );
 }
 
