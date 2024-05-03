f45a511b1646c12148426ab568d6dc69f747f2c7
sabeom <tkqja717@naver.com>
2024-05-03 14:37:00 +0900

    목표설정App - 기본기능(목록, 추가, 삭제)

---
+++ src/components/CourseGoals/CourseItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CourseItem.css';
+
+const CourseItem = ({ item, onDelete }) => {
+  return (
+    <li className="goal-item" onClick={() => onDelete(item.id)}>
+      {item.text}
+    </li>
+  );
+};
+
+export default CourseItem;
